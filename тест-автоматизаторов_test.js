184a85d2339bc92ccdc52aa0dca5d2e36235396e
nookm19 <59681683+nookm19@users.noreply.github.com>
2020-02-17 14:56:07 +0200

    Add files via upload

---
+++ тест-автоматизаторов_test.js
@@ -0,0 +1,12 @@
+Feature('Тест-автоматизаторов');
+
+Scenario('test entry', (I) => {
+	I.amOnPage('https://vk.com');
+	I.click('Ваше имя');
+	I.fillField('Ваше имя', 'somename123');;
+	I.fillField('Ваша фамилия', 'sername2135');
+	I.fillField('#container1 > table > tbody > tr > td.selector > input.selector_input.selected', '5');
+	I.fillField('#container2 > table > tbody > tr > td.selector > input.selector_input.selected', '5');
+	I.fillField('#ij_birthdate_row > div.ij_byear', '1995');
+	I.click('Продолжить регистрацию');
+});
